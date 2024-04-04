9b0f5dfbda02cfd986f9e731ee1ecc241b424719
aallm004 <abigailrosea10@gmail.com>
2024-04-04 22:42:05 +0000

    Start at the beginning....... and with what you know?

--- main.c
+++ main.c
@@ -8,10 +8,22 @@
  * 
  * Return: 0, 1
 */
-int main(argc, argv**)
+int main(int argc, char **argv)
 {
-	char **, **;
+	FILE *file;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
 	
-
-
-}
