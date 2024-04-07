9e3b782dcca896e83f37fa65484fdf950774fc0e
aallm004 <abigailrosea10@gmail.com>
2024-04-07 01:36:03 +0000

    Revert "Revert "changed malloc for buffer""
    This reverts commit b55074b2ba10ebd0e9024028b746af6b4b7141c1.

--- bytecode/003.m
+++ bytecode/003.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 push 0
 push 1
 push 2
@@ -10,9 +9,4 @@ push 4
     push 5    
       push    6        
 
-=======
-push 1
-push 2
-push 3
->>>>>>> parent of 743759e (changed malloc for buffer)
 pall
