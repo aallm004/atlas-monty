224b77aa9f55b3bda7074546681adf336f2f8c76
aallm004 <abigailrosea10@gmail.com>
2024-04-07 23:13:58 +0000

    added error and frees

--- add.c
+++ add.c
@@ -12,10 +12,19 @@ void op_add(stack_t **stack, unsigned int line_number, char **user_args)
 {
 	int x, i;
 
+	if(*stack && (*stack)->next)
+	{
 	i = delete_node(stack);
 	x = delete_node(stack);
 
-	add_node(stack, x + i, user_args);
+	add_node(stack, (x + i), user_args);
+	}
+	else
+	{
+		free(*stack);
+		free_string_array(user_args);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-	line_number = line_number;
 }
