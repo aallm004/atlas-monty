1c2379420922241ab2496719115dd0ddc789bdc7
aallm004 <abigailrosea10@gmail.com>
2024-04-08 00:11:57 +0000

    added _isspace function

---
+++ _isspace.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * _isspace- find out if something is made up entirely of whitespace
+ * @str: the string to check
+ * 
+ * Return: 0 if any not whitespace found, 1 if string is entirely whitespace
+ */
+int _isspace(char *str) 
+{
+    while (*str) 
+	{
+        if (*str != ' ' && *str != '\n' && *str != '\t')
+		{
+            return (0);
+        }
+		str++;
+    }
+    return (1);
+}
